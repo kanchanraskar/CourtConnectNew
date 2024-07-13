5f5749a827eecdd9414489766ddd04a69b39e8ae
[kanchanraskar] <[raskarkanchan1@gmail.com]>
2024-07-13 03:18:02 -0700

    login and registration component

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import BookTurf from './components/BookTurf';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import Play from './components/Play';
+import ContactUs from './components/ContactUs';
+import FAQ from './components/FAQ';
+import Login from './components/Login';
+import Footer from './components/Footer';
+import Register from './components/Register';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/play" element={<Play />} />
+        <Route path="/book" element={<BookTurf />} />
+        <Route path="/contact" element={<ContactUs/>} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/login" element={<Login />} />
+        
+     
+
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/AboutUs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutUs.css'; // Import the CSS file for styles
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Us</h2>
+      <p>
+        Sport zone
+        play hard,smile harder
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
