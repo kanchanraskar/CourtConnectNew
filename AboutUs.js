5131c6769290d97f7d439ed744313866bfa81501
Aditya Anant Ambad <126644004+Adiambad666@users.noreply.github.com>
2024-07-14 16:48:26 +0530

    AboutUs.js

---
+++ AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AboutUs.css'; // Import the CSS file for styles
+
+const AboutUs = () => {
+    return ( <
+        div className = "about-us" >
+        <
+        h2 > About Us < /h2> <
+        p >
+        "Welcome to [Your Website Name], your ultimate guide to discovering nearby playgrounds and exciting events in your area. Whether you're looking to find the perfect spot for your children to play or seeking family-friendly activities, we've got you covered. Explore our interactive map to locate playgrounds near you, complete with detailed information on amenities and facilities. Stay updated with upcoming events such as sports tournaments, community gatherings, and educational workshops hosted at these playgrounds. Discover, play, and connect with your community through [Your Website Name], your trusted resource for all things playground-related." / < /p>  <
+        /div >
+    );
+};
+
+export default AboutUs;
